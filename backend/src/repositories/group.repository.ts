7036467c17b25056925455017cdcf8c7cd5e4fcc
luansmelo <luan_decruz19@hotmail.com>
2024-03-09 11:16:36 -0300

    feat: criar repositório do grupo

--- backend/src/repositories/group.repository.ts
+++ backend/src/repositories/group.repository.ts
@@ -1,28 +1,52 @@
 import { PrismaClient } from "@prisma/client";
-import { GroupContract, GroupInput } from "@/dto/group/group.dto";
-import { GroupRepositoryContract } from "@/utils/contracts/group-contract";
 
-export class GroupRepository implements GroupRepositoryContract {
+import {
+  CreateGroupContract,
+  DeleteGroupContract,
+  FindGroupByIdContract,
+  FindGroupByNameContract,
+  FindGroupsContract,
+  UpdateGroupContract,
+  GroupModel,
+} from "@/contracts/group";
+import { CreateGroupModel } from "@/entities/group/createGroup";
+
+export class GroupRepository
+  implements
+    CreateGroupContract,
+    DeleteGroupContract,
+    FindGroupByIdContract,
+    FindGroupByNameContract,
+    FindGroupsContract,
+    UpdateGroupContract
+{
   constructor(private readonly db: PrismaClient) {}
-  async save(input: GroupContract) {
-    await this.db.group.create({
+  async save(input: CreateGroupModel): Promise<GroupModel> {
+    return this.db.group.create({
       data: input,
     });
   }
 
-  async getAll(): Promise<any> {
+  async findAll(): Promise<GroupModel[] | null> {
     return this.db.group.findMany();
   }
 
-  async getById(id: string): Promise<any> {
+  async findById(id: string): Promise<GroupModel | null> {
     return this.db.group.findUnique({ where: { id } });
   }
 
-  async deleteById(id: string): Promise<GroupInput> {
+  async findByName(name: string): Promise<GroupModel | null> {
+    return this.db.group.findUnique({ where: { name } });
+  }
+
+  async deleteById(id: string): Promise<GroupModel> {
     return this.db.group.delete({ where: { id } });
   }
 
-  async updateById(id: string, input: GroupInput): Promise<GroupInput> {
+  async updateById(
+    id: string,
+    input: Partial<CreateGroupModel>
+  ): Promise<GroupModel> {
     return this.db.group.update({ where: { id }, data: input });
   }
 }
