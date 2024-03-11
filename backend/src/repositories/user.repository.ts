94b9955cfc1e4a4eccfb0221cd43f59b9298ad67
luansmelo <luan_decruz19@hotmail.com>
2024-03-11 11:40:53 -0300

    feat: adicionar contrato de criação e busca por email no repositório do usuário

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,9 +1,11 @@
 import { CreateUserContract, UserModel } from "@/contracts/user/createUser";
-import { UserDataContract } from "@/dto/user/user.dto";
+import { FindUserByEmailContract } from "@/contracts/user/findUseryByEmail";
 import { CreateUserModel } from "@/entities/user/createUser";
 import { PrismaClient } from "@prisma/client";
 
-export class UserRepository implements CreateUserContract {
+export class UserRepository
+  implements CreateUserContract, FindUserByEmailContract
+{
   constructor(private readonly db: PrismaClient) {}
   async save(input: CreateUserModel): Promise<UserModel> {
     return this.db.user.create({
@@ -11,9 +13,7 @@ export class UserRepository implements CreateUserContract {
     });
   }
 
-  async getByEmail(email: string): Promise<UserDataContract> {
-    const db = await this.db.user.findUnique({ where: { email } });
-
-    return db;
+  async findByEmail(email: string): Promise<UserModel | null> {
+    return this.db.user.findUnique({ where: { email } });
   }
 }
