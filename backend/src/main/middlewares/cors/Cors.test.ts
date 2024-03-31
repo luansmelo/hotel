ada1f14710cccdc491bcd116ded87ce9ae4bcac0
luansmelo <luan_decruz19@hotmail.com>
2024-03-31 19:59:20 -0300

    test: testar middlewares da main

---
+++ backend/src/main/middlewares/cors/Cors.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../../config/app';
+
+describe('CORS Middleware', () => {
+
+    it('should enable CORS', async () => {
+        app.get('/test_cors', (req, res) => {
+            res.send();
+        });
+        
+        await request(app)
+            .get('/test_cors')
+            .expect('access-control-allow-origin', '*')
+            .expect('access-control-allow-methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            .expect('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token')
+            .expect('access-control-allow-credentials', 'true');
+    });
+});
