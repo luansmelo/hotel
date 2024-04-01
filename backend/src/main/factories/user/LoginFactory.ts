0e05c08baa6f6c3ad14f884de75606048c63994a
luansmelo <luan_decruz19@hotmail.com>
2024-04-01 10:04:21 -0300

    refactor: remoção do email validator que não será utilizado no login factory

--- backend/src/main/factories/user/LoginFactory.ts
+++ backend/src/main/factories/user/LoginFactory.ts
@@ -1,20 +1,14 @@
 import { CRYPTOGRAPHY_SALTING_ROUNDS } from "@/config/constants";
 import env from "@/config/env";
 import { CreateAuthUseCase } from "@/data/usecases/user/CreateAuthUseCase";
-
 import { BcryptAdapter } from "@/infra/cryptography";
 import { UserRepository } from "@/infra/db/mysql/user/UserRepository";
-
 import { JwtAdapter } from "@/infra/token";
 import { LoginController } from "@/presentation/controllers/user/LoginController";
-
-import { EmailValidatorAdapter } from "@/utils/EmailValidatorAdapter";
 import { makeLoginValidationFactory } from "./LoginValidationFactory";
 
 export function makeLoginController(): LoginController {
   const repository = new UserRepository();
-
-  const emailValidator = new EmailValidatorAdapter();
 
   const hashed = new BcryptAdapter(CRYPTOGRAPHY_SALTING_ROUNDS);
 
@@ -22,7 +16,6 @@ export function makeLoginController(): LoginController {
 
   const createAuth = new CreateAuthUseCase(
     repository,
-    emailValidator,
     hashed,
     jwtAdapter
   );
